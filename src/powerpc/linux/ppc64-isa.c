ed4fae8baac0b4d1ef17c13327e87ad2e942e57a
manjul@linux.vnet.ibm.com <manjulmohan@trout.aus.stglabs.ibm.com>
2024-03-06 00:07:38 -0600

    Removing explicit conversion of cpuinfo features macro to UINT32_C

--- src/powerpc/linux/ppc64-isa.c
+++ src/powerpc/linux/ppc64-isa.c
@@ -7,8 +7,8 @@ void cpuinfo_ppc64_linux_decode_isa_from_proc_cpuinfo(
 	uint32_t features, uint32_t features2,
 	struct cpuinfo_powerpc_isa isa[restrict static 1])
 {
-	isa->vsx=!!(features&UINT32_C(CPUINFO_POWERPC_LINUX_FEATURE_HAS_VSX));
+	isa->vsx=!!(features & CPUINFO_POWERPC_LINUX_FEATURE_HAS_VSX);
 	//isa.vmx=features&UINT32_C(?);
-	isa->htm=!!(features2&UINT32_C(CPUINFO_POWERPC_LINUX_FEATURE_HTM));
-	isa->mma=!!(features2&UINT32_C((CPUINFO_POWERPC_LINUX_FEATURE_HAS_MMA));
+	isa->htm=!!(features2 & CPUINFO_POWERPC_LINUX_FEATURE_HTM);
+	isa->mma=!!(features2 & CPUINFO_POWERPC_LINUX_FEATURE_HAS_MMA);
 }
